35a6f9d95bb5223592de8a5039a954ec8a46f26f
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-10-21 13:41:06 +0200

    UsersController added

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager _userManager;
+
+        public UsersController(UserManager userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
