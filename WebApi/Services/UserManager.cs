52a2891890c9ba08b16a91021f83ebe93dd81759
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-10-21 13:11:43 +0200

    dependency injection added

---
+++ WebApi/Services/UserManager.cs
@@ -0,0 +1,14 @@
+﻿using WebApi.Models.Data;
+
+namespace WebApi.Services
+{
+    public class UserManager
+    {
+        private readonly DataContext _context;
+
+        public UserManager(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
